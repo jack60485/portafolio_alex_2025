a42d34b5ac8c7e718df042f099686466a003d260
Alexander Cruz <jack60485@gmail.com>
2025-08-06 16:42:21 +1000

    👷 Mover index.html a templates

--- backend/portfolio_backend/settings.py
+++ backend/portfolio_backend/settings.py
@@ -76,7 +76,8 @@ ROOT_URLCONF = 'portfolio_backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'staticfiles/frontend')],
+        'DIRS': [os.path.join(BASE_DIR, 'backend', 'portfolio_backend', 'templates')],
+
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
