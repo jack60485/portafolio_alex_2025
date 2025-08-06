1c47ddac90cdcc83458637813fc7b58db1b9b4ca
Alexander Cruz <jack60485@gmail.com>
2025-08-06 15:03:43 +1000

    Corrijo startCommand y confirmo render.yaml

--- backend/portfolio_backend/wsgi.py
+++ backend/portfolio_backend/wsgi.py
@@ -11,7 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.portfolio_backend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio_backend.settings')
+
 
 
 
